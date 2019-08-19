650950375166ff90c7bea67ec93f4c06d5c0c150
yanagi_prg <takayanagi.prg@gmail.com>
2019-08-20 00:31:24 +0900

    create routing

--- react-practice/CompleteReactDeveloper/e-commerce/src/App.js
+++ react-practice/CompleteReactDeveloper/e-commerce/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route, } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = props => {
+  return (
+    <div>
+      <button onClick={() => props.history.push('/topics')}>Topics</button>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}  />
+        <Route path='/hats' component={HatsPage}  />
+      </Switch>
     </div>
   );
 }
--- react-practice/CompleteReactDeveloper/e-commerce/src/components/menu-item/menu-item.component.jsx
+++ react-practice/CompleteReactDeveloper/e-commerce/src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MemuItem = ({title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}> 
+const MemuItem = ({title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}> 
     <div 
       className='background-image' 
       style={{
@@ -17,4 +18,4 @@ const MemuItem = ({title, imageUrl, size }) => (
   </div>
 );
 
-export default MemuItem;
+export default withRouter(MemuItem);
