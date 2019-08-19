fb28c5829c4fe7fa70b0063b1d8660fb9f68b67c
yanagi_prg <takayanagi.prg@gmail.com>
2019-08-20 02:44:47 +0900

    create header

--- react-practice/CompleteReactDeveloper/e-commerce/src/App.js
+++ react-practice/CompleteReactDeveloper/e-commerce/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = props => {
   return (
@@ -17,6 +18,7 @@ const HatsPage = props => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}  />
         <Route path='/shop' component={ShopPage}  />
