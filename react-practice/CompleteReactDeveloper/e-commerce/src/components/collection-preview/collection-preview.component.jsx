c55c32a13089c37ab2c238a35bb609d6b9f0c5f5
yanagi_prg <takayanagi.prg@gmail.com>
2019-08-20 02:21:07 +0900

    create collection-item

--- react-practice/CompleteReactDeveloper/e-commerce/src/components/collection-preview/collection-preview.component.jsx
+++ react-practice/CompleteReactDeveloper/e-commerce/src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({title, items}) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {
-        items
+      {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))
-      }
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} { ...otherItemProps} />
+        ))}
     </div>
   </div>
-)
+);
 
 export default CollectionPreview;
