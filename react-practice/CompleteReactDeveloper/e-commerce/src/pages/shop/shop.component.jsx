d72d513b766b53910fc42569e399b23db4604bed
yanagi_prg <takayanagi.prg@gmail.com>
2019-08-25 12:28:00 +0900

    converted local state of directory & shop with redux

--- react-practice/CompleteReactDeveloper/e-commerce/src/pages/shop/shop.component.jsx
+++ react-practice/CompleteReactDeveloper/e-commerce/src/pages/shop/shop.component.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collctions-overview.component';
 
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    // const collections = this.state;
-    return(
-      <div className='shop-page'>
-        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
 
 export default ShopPage;
